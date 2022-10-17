64bbf7e74c1b8f3cc2de40404819762105bf0e2a
Wojciech Sowiński <wojciech.sowinski@owliedev.pl>
2022-10-17 16:14:24 +0200

    url change

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@
 
 const config = {
     // serverUrl: 'http://serv.owliedev.pl:80/'
-    serverUrl: 'https://tinder-clone-enzv.vercel.app/'
+    serverUrl: 'https://tinder-clone-enzv.vercel.app'
     // defaultUserImg: defaultImg
 };
 
