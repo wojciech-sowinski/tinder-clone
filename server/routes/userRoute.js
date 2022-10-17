f290e4a8173f8a283e2339239ab4cefe1df42392
Wojciech Sowiński <wojciech.sowinski@owliedev.pl>
2022-10-17 14:04:58 +0200

    change vercel .json

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -76,15 +76,15 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
 
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+    // res.setHeader('Access-Control-Allow-Credentials', true)
+    // res.setHeader('Access-Control-Allow-Origin', '*')
+    // // another common pattern
+    // // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    // res.setHeader(
+    //   'Access-Control-Allow-Headers',
+    //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    // )
 
     const {
         email,
