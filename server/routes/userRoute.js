a9e2f7d23158f7f8d2f8463e6e27117d06ae9695
Wojciech Sowiński <wojciech.sowinski@owliedev.pl>
2022-10-17 14:01:56 +0200

    change cors option

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -76,6 +76,16 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
 
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
     const {
         email,
         password
