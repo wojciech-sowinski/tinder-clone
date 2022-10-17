c9e4615ed0ac7f6f15c404cdb1c4bcebae0c10eb
Wojciech Sowiński <wojciech.sowinski@owliedev.pl>
2022-10-17 12:29:25 +0200

    1

--- server/app.js
+++ server/app.js
@@ -33,15 +33,15 @@ mongoose.connect(mongoDbUrl)
 
 
 const corsOption = {
-    // origin: 'http://localhost:3000',
-    origin: 'http://owliedev.pl',
+    origin: 'http://localhost:3000',
+    // origin: 'http://owliedev.pl',
     credentials: true
 }
 
 //middleware
 app.use(cors(corsOption));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", corsOption.origin);
+    res.header("Access-Control-Allow-Origin", req.header('Origin'));
     res.header("Access-Control-Allow-Credentials", true);
     res.header(
       "Access-Control-Allow-Headers",
