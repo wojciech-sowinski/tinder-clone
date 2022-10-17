9feb0d56b12f40eedf626d7280dc7de933b4e378
Wojciech Sowiński <wojciech.sowinski@owliedev.pl>
2022-10-17 16:18:55 +0200

    123

--- client/src/components/UploadImg.jsx
+++ client/src/components/UploadImg.jsx
@@ -31,7 +31,7 @@ function UploadImg() {
         const formData = new FormData();
         formData.append('image', compressedFile);
 
-        axios.post(config.serverUrl + 'upload', {
+        axios.post(config.serverUrl + 'upload1', {
             data: formData,
             headers: { 'Content-type': 'multipart/form-data' },
             withCredentials: true
